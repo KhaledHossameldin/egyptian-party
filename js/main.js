434584e4160a0b77ecc33a267a77a887f6df31ac
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-08-14 17:39:03 +0300

    added singers section UI + logic and fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $('#open-drawer').click(function () {
-    //TODO: open drawer logic
     $('#drawer').animate({width: '250px'}, 500);
     $('#header-content').animate({marginLeft: '250px'}, 500);
 });
@@ -7,4 +6,9 @@ $('#open-drawer').click(function () {
 $('#close-drawer').click(function () {
     $('#drawer').animate({width: '0px'}, 500);
     $('#header-content').animate({marginLeft: '0px'}, 500);
+});
+
+$('.singer-header').click(function (e) {
+    $('.singer-body').not($(this).next()).slideUp(500);
+    $(this).next().slideToggle(500);
 });
